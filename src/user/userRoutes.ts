a1726d0daab44c089d4944ad4886405c1d5790f5
wavezboy <sanusifatah16@gmail.com>
2024-06-16 16:59:14 +0100

    sign up routes, login routes, logout routes

--- src/user/userRoutes.ts
+++ src/user/userRoutes.ts
@@ -7,3 +7,7 @@ const router = express.Router();
 router.get("/autologin", checkAuthUser, userController.autoLogin);
 router.get("/", userController.getAllUser);
 router.get("/:id", userController.getUser);
+
+router.post("/signup", userController.signUp);
+router.post("/login", userController.logIn);
+router.post("/logout", userController.logOut);
