f59ff483ba2708709072e5d443f203ce488bcc61
wavezboy <sanusifatah16@gmail.com>
2024-06-18 11:18:37 +0100

    new routes update

--- src/category/routes.ts
+++ src/category/routes.ts
@@ -1,55 +1,12 @@
-import { RequestHandler } from "express";
-import { category, createCategoryType } from "../utils/types";
-import { PrismaClient } from "@prisma/client";
+import express from "express";
+import * as categoryController from "./controller";
 
-const prisma = new PrismaClient();
-export const createCategory: RequestHandler<
-  unknown,
-  unknown,
-  createCategoryType,
-  unknown
-> = async (req, res) => {
-  const { categories } = req.body;
-  const userId = req.session.userId;
+const router = express.Router();
 
-  try {
-    if (!userId) {
-      return res.status(500).json("login to select a catgeory");
-    }
-    if (categories.length == 0) {
-      return res.status(500).json("select a category to get started");
-    }
+router.post("/create-category", categoryController.createCategory);
 
-    const userBudget = await prisma.budget.findFirst({
-      where: { userId: userId },
-    });
+router.put("/update-category", categoryController.updateCategory);
 
-    if (!userBudget) {
-      return res
-        .status(500)
-        .json("you need to have a budget to create a category");
-    }
+router.get("/get-categories", categoryController.getCategories);
 
-    const budgetId = userBudget.id;
-
-    const categoriesWithBudgetId = categories.map((category) => ({
-      ...category,
-      budgetId,
-    }));
-
-    const result = await prisma.category.createMany({
-      data: categoriesWithBudgetId,
-    });
-
-    const newCategories = await prisma.category.findMany({
-      where: {
-        budgetId,
-      },
-    });
-
-    return res.status(200).json(newCategories);
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json(error.message);
-  }
-};
+export default router;
